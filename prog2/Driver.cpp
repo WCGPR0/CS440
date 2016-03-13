e015f7ebfb9254c85e36f900b23176990d1f14f8
Victor Wu <vwu4@cs.binghamton.edu>
2016-03-13 00:43:35 -0500

    Whao, what did I doooooo

--- prog2/Driver.cpp
+++ prog2/Driver.cpp
@@ -4,6 +4,8 @@ int main(int argc, char* argv[]) {
 //Map<int, double> int_double_map {{1, 1.0}, {3, 5.67}, {13, 6.9}};
     cs540::Map<int, int> m;
     m.insert({10, 10});
+    cs540::Map<int, int>::Iterator it = m.begin();
+    *it;
 //    m.insert({3, 3});
 //    m.insert({20, 20});
 //    m.insert({15, 15}); 
--- prog2/Map.hpp
+++ prog2/Map.hpp
@@ -30,47 +30,71 @@ public:
 		~Iterator() {}	
 		Iterator& operator++(){ node_p = node_p->next[0]; return *this; }
 		Iterator operator++(int) { Iterator it = *this; ++*this; return it; }
-		Iterator& operator--() { node_p = node_p->prev; return *this; }
+		Iterator& operator--() { node_p = node_p->prev[0]; return *this; }
 		Iterator operator--(int) { Iterator it = *this; --*this; return it; }
-		ValueType &operator*() const { 
-			return node_p == nullptr ? std::pair<const Key_T, Mapped_T>(NULL,NULL) : std::pair<const Key_T, Mapped_T>(node_p->key, node_p->value); } 		
+		ValueType &operator*() const {
+		std::pair<const Key_T&, Mapped_T&> p(node_p->base.first, node_p->base.second);	
+			return node_p->base;
+		//return node_p == nullptr ? ValueType(NULL,NULL) : ValueType(key,value);
+		} 		
 		ValueType *operator->() const {
-			return node_p == nullptr ? std::pair<const Key_T, Mapped_T>(NULL,NULL) : std::pair<const Key_T, Mapped_T>(node_p->key, node_p->value); } 		
+			return &node_p->base;
+		}	
 	
 	friend bool operator==(const Iterator &it1, const Iterator &it2) {return it1.node_p == it2.node_p;}
 
 	friend bool operator!=(const Iterator &it1, const Iterator &it2) {return !(it1 == it2);}
 	};
-	struct ConstIterator : public Iterator {	
+	struct ConstIterator : public Iterator {
+		ConstIterator (typename SkipList::Node& node) : Iterator(node) {}	
 		const std::pair<const Key_T, Mapped_T> &operator*() const;
 		const std::pair<const Key_T, Mapped_T> *operator->() const;
 
 	};
 	struct ReverseIterator : public Iterator {
-		ReverseIterator& operator++() {}
-		ReverseIterator operator++(int) {}
-		ReverseIterator& operator--() {}
-		ReverseIterator operator--(int) {}
+		ReverseIterator (typename SkipList::Node& node) : Iterator(node) {}
+		ReverseIterator& operator++() {Iterator::operator--();}
+		ReverseIterator operator++(int) {
+			Iterator::operator--(0);
+			/*
+			ReverseIterator result = *this;
+  	      		++(*this);
+    			return result
+			*/
+		}
+		ReverseIterator& operator--() {Iterator::operator++();}
+		ReverseIterator operator--(int) {
+			Iterator::operator++(0);
+			/*
+			ReverseIterator result = *this;
+    			--(*this);
+    			return result;
+			*/
+			}
 	};
 
 	class SkipList {
 		//Forward Delcarations	
 public:
 		struct Node {
-			Key_T key[MAX_LEVEL] = {0}; //Tiers for probability hits	i
-			Mapped_T value;
+			Key_T key[MAX_LEVEL-1] = {0}; //Tiers for probability hits	
+			ValueType base;	
 			Node *next[MAX_LEVEL], *prev[MAX_LEVEL];
-			Node(Key_T key, Mapped_T value, Node* next = nullptr) : key{key}, value(value), next{next} { if (this->next[0] != nullptr) this->next[0]->prev[0] = this; }
+			Node(Key_T key, Mapped_T value, Node *next = nullptr) : next{next}, base(key, value) { 
+				if (this->next[0] != nullptr) this->next[0]->prev[0] = this; 
+			}
 		};
 
 public:
 		Node *head = nullptr, *tail = nullptr;
 		Key_T min, max;
 
-		SkipList() {}
+		SkipList() {
+		//	tail = new Node(maxKey, maxValue);
+		}
 		SkipList(Key_T minKey, Key_T maxKey, Mapped_T minValue, Mapped_T maxValue): min(min), max(max){
-			//tail = new Node(maxKey, maxValue);
-			//head = new Node(minKey, minValue, &*tail);
+			tail = new Node(maxKey, maxValue);
+			head = new Node(minKey, minValue, &*tail);
 		}
 		Node* insert(const Key_T key, Mapped_T newValue) {
 			// CONSIDER THE CASE: KEY > END VALUE. !!! IMPORTANT (infinite loop)
@@ -81,19 +105,23 @@ public:
 			if (head == nullptr) {
 				head = new Node(key, newValue, &*tail);
 				insertNodePointer = head;
-				for (int tier = 0; tier < MAX_LEVEL; ++tier)
+				for (int tier = 0; tier < MAX_LEVEL-1; ++tier)
 					insertNodePointer->key[tier] = key;	
 			}
 			else {
 			currentNodePointer = head;
-			for (int tier = MAX_LEVEL; tier >= 1; --tier) {
+			for (int tier = MAX_LEVEL-1; tier >= 1; --tier) {
 				currentNodePointer = currentNodePointer->next[tier];
 				while (currentNodePointer->next[tier])
 					if (currentNodePointer->key[tier] > key) {
 						insertNodePointer = currentNodePointer; 
-						break;
+						goto INSERTFOUND;	
 					}	
-			}	
+			}
+			while (currentNodePointer->next[0])
+				if (currentNodePointer->base.first > key)
+					insertNodePointer = currentNodePointer;
+INSERTFOUND:	
 			insertNodePointer->prev[0]->next[0] = new Node(key, newValue, insertNodePointer); //Sets previous node to point to newly constructed node, and update properties of next node
 			insertNodePointer = insertNodePointer->prev[0]->next[0];
 			for (int i = 1; rand() % 2; ++i)
@@ -114,8 +142,8 @@ public:
 		map = new SkipList();
 	}
 	Map(std::initializer_list<ValueType> pair) : Map() {
-		for (auto i = pair.begin(); i != pair.end(); ++pair)
-			insert(i->first, i->second);
+		for (auto i = pair.begin(); i != pair.end(); ++i)
+			map->insert(i->first, i->second);
 	}
 	Map(const Map&) {
 		//this.valueType = Map.valueType;
@@ -140,28 +168,55 @@ public:
 	Iterator end() {
 		return Iterator(*(map->tail)); 
 	}
-/*
-	typename SkipList::ConstIterator begin() const {
-		return ConstIterator(map.begin());
+
+	ConstIterator begin() const {
+		return ConstIterator(*(map->head));
 	}
-	typename SkipList::ConstIterator end() const {
-		return ConstIterator(map.end());
+	ConstIterator end() const {
+		return ConstIterator(*(map->tail));
 	}
-	typename SkipList::ReverseIterator rbegin() {
-		return ReverseIterator(map.begin());
+	ReverseIterator rbegin() {
+		return ReverseIterator(*(map->tail));
 	}
-	typename SkipList::ReverseIterator rend() {
-		return ReverseIterator(map.begin());
+	ReverseIterator rend() {
+		return ReverseIterator(*(map->head));
 	}
-	typename SkipList::Iterator find(const Key_T &x) {
-		map.find(x);
+	Iterator find(const Key_T &x) {
+		Iterator it = this->begin();	
+		while (it != this->end()) {
+			if(it->first == x) break;
+			++it;
+		}
+		return it;
 	}
-	typename SkipList::ConstIterator find(const Key_T &x) const {
-		map.find(x);
-	}*/
-	Mapped_T &at(const Key_T &x);
-	const Mapped_T &at(const Key_T &x) const;
-	Mapped_T &operator[](const Key_T &x);
+	ConstIterator find(const Key_T &x) const {
+		ConstIterator it = this->begin();
+		while (it != this->end()) {
+			if (it->first ==x) break;
+			it++;
+		}
+		return it;	
+	}
+	Mapped_T &at(const Key_T &x) {
+		Iterator it = this->begin();
+		while (it != this->end()) {
+			if (it.node_p->base.first == x) return it.node_p->base.second;
+			++it;
+		}
+		return it.node_p->base.second; //Will throw an error at this stage, meaning no elements found
+	}
+	const Mapped_T &at(const Key_T &x) const {
+		Iterator it = this->begin();
+		while (it != this->end()) {
+			if (it.node_p->base.first == x) return it.node_p->base.second;
+			++it;
+		}
+		return it.node_p->base.second; //Will throw an error at this stage, meaning no elements found
+		
+	}
+	Mapped_T &operator[](const Key_T &x) {
+		return this->at(x);
+	}
 	//std::pair<typename SkipList::Iterator, bool> insert(const std::pair<const Key_T, Mapped_T>& pair) {SkipList::insert(pair.first, pair.second); };
 	std::pair<Iterator, bool> insert(const ValueType &pair) {
 		Key_T key = pair.first;
@@ -184,7 +239,7 @@ public:
 		}
 	}
 	bool operator==(const Map& map) {
-		Iterator it(map.head), it2(this.head);
+		Iterator it(map.head), it2(this->head);
 		while (it != map.end()) {
 			if (it->first != it2->second) return false;
 			++it;
